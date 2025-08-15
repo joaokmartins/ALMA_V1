f2f0ddc72826badb9e0cb2e7f2d55bace9433bcc
joaokmartins <joaomartins@live.it>
2025-08-14 22:57:08 -0400

    feat: Adiciona SimpleTokenizerV2 para tratar palavras desconhecidas
    Este commit introduz o `SimpleTokenizerV2` para melhorar o tratamento de texto,
    resolvendo o problema de `KeyError` ao encontrar palavras fora do vocabulário
    durante a tokenização [1].

--- src/data_loader.py
+++ src/data_loader.py
@@ -25,28 +25,43 @@ for i, item in enumerate(vocab.items()):
     if i >= 50:
         break    
 
-class SimpleTokenizerV1:
-    def __init__(self, vocab):
-        self.str_to_int = vocab #1
-        self.int_to_str = {i:s for s,i in vocab.items()} #2
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
 
-    def encode(self, text): #3
-        preprocessed = re.split(r'([,.?_!"()\']|--|\s+)', text)
+print(len(vocab.items()))
+
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item) 
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
         preprocessed = [
-        item.strip() for item in preprocessed if item.strip()
-            ]
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        preprocessed = [item if item in self.str_to_int             #1
+                        else "<|unk|>" for item in preprocessed]
+      
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
-        
-    def decode(self, ids): #4
+   
+    def decode(self, ids):
         text = " ".join([self.int_to_str[i] for i in ids])
-        
-        text = re.sub(r"\s+([,.?!\"()\\'])", r"\1", text) #5
+
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
 
-tokenizer = SimpleTokenizerV1(vocab)
-text = """"It's the last he painted, you know,"
-Mrs. Gisburn said with pardonable pride."""
-ids = tokenizer.encode(text)
-print(ids)
-print(tokenizer.decode(ids))
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+
+tokenizer = SimpleTokenizerV2(vocab)
+print(tokenizer.encode(text))
+
+print(tokenizer.decode(tokenizer.encode(text)))
