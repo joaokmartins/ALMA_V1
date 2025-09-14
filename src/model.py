00be150d734218659711023eb4947fea6501e6da
joaokmartins <joaomartins@live.it>
2025-09-14 13:37:11 -0400

    Finaliza GPT: Implementa GELU, LayerNorm, TransformerBlock, arquitetura GPT e função básica de geração de texto.

--- src/model.py
+++ src/model.py
@@ -422,6 +422,22 @@ layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
 out = layer(batch_example)
 print(out)
 
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print("Normalized layer outputs:\n", out_norm)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+torch.set_printoptions(sci_mode=False)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
 # Implementa a **normalização de camada**, ajustando a média para 0 
 # e a variância para 1 [1].
 # Utiliza parâmetros aprendíveis (`scale`, `shift`) para otimização e `eps` 
@@ -447,3 +463,228 @@ var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
 torch.set_printoptions(sci_mode=False)
 print("Mean:\n", mean)
 print("Variance:\n", var)
+
+#Implementa a função de ativação GELU
+class GELU(nn.Module):
+    def __int__(self):
+        super().__init__()
+    
+    def forward(self,x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / 
+torch.pi)) *
+            (x + 0.044715 * torch.pow(x,
+3))
+        ))
+#Um módulo de rede neural feedforward
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * 
+cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"],
+cfg["emb_dim"]),
+        )
+    
+    def forward(self, x):
+        return self.layers(x)
+    
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2, 3, 768)  
+out = ffn(x)
+print(out.shape)
+
+#Uma rede neural para ilustrar conexões de atalho
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([  
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]),
+            GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]),
+            GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]),
+            GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]),
+            GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]),
+            GELU())
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            layer_output = layer(x)  
+            if self.use_shortcut and x.shape == layer_output.shape:  
+                x = x + layer_output
+            else:
+                x = layer_output
+        return x
+
+layer_sizes = [3, 3, 3, 3, 3, 1]
+sample_input = torch.tensor([[1., 0., -1.]])
+torch.manual_seed(123)  
+model_without_shortcut = ExampleDeepNeuralNetwork(
+    layer_sizes, use_shortcut=False
+)
+
+#Implementa uma função que calcula os gradientes na passagem reversa do modelo
+def print_gradients(model, x):
+    output = model(x)  
+    target = torch.tensor([[0.]])
+
+    loss = nn.MSELoss()
+    loss = loss(output, target)  
+
+    loss.backward() 
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
+
+print_gradients(model_without_shortcut, sample_input)
+
+torch.manual_seed(123)
+model_with_shortcut = ExampleDeepNeuralNetwork(
+    layer_sizes, use_shortcut=True
+)
+print_gradients(model_with_shortcut, sample_input)
+
+#O componente do bloco transformador do GPT
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut 
+
+        shortcut = x 
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut 
+        return x
+
+torch.manual_seed(123)
+x = torch.rand(2, 4, 768) #1
+block = TransformerBlock(GPT_CONFIG_124M)
+output = block(x)
+
+print("Input shape:", x.shape)
+print("Output shape:", output.shape)
+
+#Implementação da arquitetura do modelo GPT
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+        *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+        cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(
+        torch.arange(seq_len, device=in_idx.device)
+        )
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+#Inicializa o modelo GPT de 124 milhões de parâmetros usando o dicionário GPT_CONFIG_ 124M 
+# que passamos para o parâmetro cfg e o alimentamos com a entrada de texto em 
+# lote que criamos anteriormente
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+out = model(batch)
+print("Input batch:\n", batch)
+print("\nOutput shape:", out.shape)
+print(out)
+
+#Usando o método numel(), abreviação de “número de elementos”, podemos coletar 
+# o número total de parâmetros nos tensores de parâmetros do modelo:
+total_params = sum(p.numel() for p in model.parameters())
+print(f"Total number of parameters: {total_params:,}")
+
+print("Token embedding layer shape:", model.tok_emb.weight.shape)
+print("Output layer shape:", model.out_head.weight.shape)
+
+#Remove a contagem de parâmetros da camada de saída da contagem total do 
+# modelo GPT-2 de acordo com o peso vinculado
+total_params_gpt2 = (
+    total_params - sum(p.numel()
+    for p in model.out_head.parameters())
+)
+print(f"Number of trainable parameters "
+      f"considering weight tying: {total_params_gpt2:,}"
+)
+
+#Calcula os requisitos de memória dos 163 milhões de parâmetros em 
+# nosso objeto GPTModel
+total_size_bytes = total_params * 4  
+total_size_mb = total_size_bytes / (1024 * 1024)  
+print(f"Total size of the model: {total_size_mb:.2f} MB")
+
+#Uma função para o modelo GPT para gerar texto
+def generate_text_simple(model, idx,  
+                        max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]  
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]   
+        probas = torch.softmax(logits, dim=-1)  
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True) 
+        idx = torch.cat((idx, idx_next), dim=1) 
+        
+    return idx
+
+#Codifica o contexto de entrada em IDs de token
+start_context = "Hello, I am"
+encoded = tokenizer.encode(start_context)
+print("encoded:", encoded)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0) #1
+print("encoded_tensor.shape:", encoded_tensor.shape)
+
+#Função #generate_text_simple com o contexto "Olá, eu sou" como entrada do modelo
+#Desabilita o dropout, pois não estamos treinando o modelo
+model.eval()  
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor,
+    max_new_tokens=6,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+print("Output:", out)
+print("Output length:", len(out[0]))
+
+#Usando o método .decode do tokenizador, podemos converter os IDs de volta em texto
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
